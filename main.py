d69745fe305fa5b3651b0d6883e5271f559e618c
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2024-01-27 06:41:15 +0900

    Add class RetrievalAgent

--- main.py
+++ main.py
@@ -6,56 +6,57 @@ from langchain_community.vectorstores.faiss import FAISS
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
+load_dotenv()
 
-def create_agent():
-    texts = [
-        "私の趣味は読書です。",
-        "私の好きな食べ物はカレーです。",
-        "私の嫌いな食べ物は饅頭です。",
-    ]
 
-    vectorstore = FAISS.from_texts(
-        texts, embedding=OpenAIEmbeddings(model="text-embedding-ada-002")
-    )
-    retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
-
-    tool = create_retriever_tool(
-        retriever,
-        "search_about_me",
-        "Searches and returns information about me.",
-    )
-    tools = [tool]
-
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", "You are a helpful assistant."),
-            MessagesPlaceholder(variable_name="chat_history", optional=True),
-            ("human", "{input}"),
-            MessagesPlaceholder(variable_name="agent_scratchpad"),
+class RetrievalAgent:
+    def __init__(self):
+        texts = [
+            "私の趣味は読書です。",
+            "私の好きな食べ物はカレーです。",
+            "私の嫌いな食べ物は饅頭です。",
         ]
-    )
 
-    llm = ChatOpenAI(model="gpt-3.5-turbo-1106")
+        vectorstore = FAISS.from_texts(
+            texts, embedding=OpenAIEmbeddings(model="text-embedding-ada-002")
+        )
+        retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
 
-    memory = ConversationBufferMemory(
-        input_key="input", memory_key="chat_history", return_messages=True
-    )
+        retriever_tool = create_retriever_tool(
+            retriever,
+            "search_about_me",
+            "Searches and returns information about me.",
+        )
+        tools = [retriever_tool]
 
-    agent = create_openai_tools_agent(llm, tools, prompt)
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", "You are a helpful assistant."),
+                MessagesPlaceholder(variable_name="chat_history", optional=True),
+                ("human", "{input}"),
+                MessagesPlaceholder(variable_name="agent_scratchpad"),
+            ]
+        )
 
-    return AgentExecutor(agent=agent, tools=tools, memory=memory)
+        llm = ChatOpenAI(model="gpt-3.5-turbo-1106")
+
+        memory = ConversationBufferMemory(
+            input_key="input", memory_key="chat_history", return_messages=True
+        )
+
+        agent = create_openai_tools_agent(llm, tools, prompt)
+
+        self._agent_chain = AgentExecutor(agent=agent, tools=tools, memory=memory)
+
+    def run(self, user_message: str) -> str:
+        result = self._agent_chain.invoke({"input": user_message})
+        return result["output"]
 
 
 def main():
-    load_dotenv()
-
-    agent_chain = create_agent()
-
-    while True:
-        user_message = input("You: ")
-        result = agent_chain.invoke({"input": user_message})
-        ai_message = result["output"]
-        print(f"AI: {ai_message}")
+    agent = RetrievalAgent()
+    ai_message = agent.run("私の趣味は何ですか？")
+    print(ai_message)
 
 
 if __name__ == "__main__":
